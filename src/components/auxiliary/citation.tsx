e43435fbbe5d296524b95b29bf97fd6d977988f4
aboyle <aboyle@student.ethz.ch>
2026-04-29 02:48:36 +0200

    booktitle-abbr added still need to improve colours

--- src/components/auxiliary/citation.tsx
+++ src/components/auxiliary/citation.tsx
@@ -28,28 +28,41 @@ function Citation(props: CitationProps) {
             <a className={`${style}`} href={URL} target="_blank">{data.join(" ")}</a>
         )
     }
-    const handleBookTitle = (field : any) : JSX.Element => {
+    const handleBookTitle = (field : any, year : string) : JSX.Element => {
         const data = handleWrapperStringField(field);
+        const bookTitle = data.join(" ");
+        // TODO set up clean table for converting book title to venue shorthand 
+        let venue = "";
+        if (bookTitle.includes("Empirical")) {
+            venue = "EMNLP";
+        } else if (bookTitle.includes("VIS")) {
+            venue = "IEEE VIS";
+        }
+        console.log("Book title:", bookTitle, "Venue:", venue, "Year:", year);
+        venue += " " + year.slice(2,4);
         return (
-            <p className="italic leading-none font-thin">{data.join(" ")}</p>
+            <p className=" inline">
+                <span className=" border-1 rounded-md px-1 py-[2px] mr-1 border-slate-300 w-fit text-nowrap leading-none font-light text-xs booktitle-abbr">{venue}</span>
+                <span className="italic leading-none font-thin">{bookTitle}</span>
+            </p>
         )
     }
     const handleAuthors = (field : any) : JSX.Element => {
         const authors = field.authors$;
         const lastIndex = authors.length - 1;
         return (
-            <span className="inline leading-none author font-light">
+            <p className="inline leading-none author font-light">
                 {authors.map((author: any, index: number) => (
                    <>
                    {author.firstNames$[0] == "Alan" ? <span key={index} className="font-normal underline" >{author.firstNames$.map((name: string, index : number) => index == 0 ? name : name[0].charAt(0)+".").join(" ")} {author.lastNames$.join("")}</span> : <>{index == lastIndex ? " and " : ""}{author.firstNames$.join(" ")} {author.lastNames$.join("")}</>}{index < lastIndex ? ", " : ""}
                    </>
                 ))}
 
-            </span>        )
+            </p>        )
     }
 
     const URL = handleWrapperStringField(bibEntry.getField("url")).join("");
-    const date = handleWrapperStringField(bibEntry.getField("year"))[0]
+    const date = String(handleWrapperStringField(bibEntry.getField("year"))[0]);
 
     return (
         <div className="flex flex-col items-start justify-start w-full h-fit">
@@ -58,8 +71,8 @@ function Citation(props: CitationProps) {
                 <p className="date leading-none font-thin text-3xl">{date}</p>
             </div>
             <div className="mt-[-7px] w-5/6 h-fit">
-                {handleAuthors(bibEntry.getField("author"))}
-                {handleBookTitle(bibEntry.getField("booktitle"))}
+                {handleAuthors(bibEntry.getField("author"))} <br/>
+                {handleBookTitle(bibEntry.getField("booktitle"), date)}
                 <div className="mt-2 h-fit flex flex-row gap-2">
                 {Array.from(linkMap.entries()).map(([label, url]) => (
                     <a key={label} href={url} target="_blank" className="border-1 px-2 text-sm rounded-lg hover:invert transition duration-200 ease-in-out">
